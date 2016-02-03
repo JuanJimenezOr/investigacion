81e53087b8223dc413a21ed8336de919f7f32dea
Eduardo Fregoso <jeedfregosoco@ittepic.edu.mx>
2016-02-02 21:54:55 -0700

    Correcion en el main

--- BorderLayout.java
+++ BorderLayout.java
@@ -1,14 +1,23 @@
-import javax.swing.JFrame;//teniamos que importar la libreria y extender la clase
+import javax.swing.JFrame;
+import java.awt.BorderLayout;//libreria necesaria para el Layout
 
 public class BorderLayout extends JFrame{
+
+
+	private void ventana()//Lo del main va en esta función, la llamaré ventana
+	{
+		//tamaño de la ventana
+        	 this.setBounds(10,40,350,280);
+         	//creacion y establecimiento del gestor de organización
+         	BorderLayout bd = new BorderLayout();
+         	this.setLayout(bd);
+		
+	}
 	
-	public static void main(String ar[]){
-	 
-	 //tamaño de la ventana
-	 this.setBounds(10,40,350,280);
-	 //creacion y establecimiento del gestor de organización
-	 BorderLayout bd = new BorderLayout();
-	 this.setLayout(bd); 
+	public static void main(String ar[])
+	{
+		BorderLayout = m BorderLayout();
+		m.BorderLayout();
 	
 	}//main
 }//class
