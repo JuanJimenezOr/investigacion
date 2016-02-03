9f830819b9f589ed0d14956c01dce34ab0c3f372
kealmendezsa@hotmail.com <kealmendezsa@hotmail.com>
2016-02-02 21:36:20 -0700

    Codigo de creacion ventana, checar

---
+++ BorderLayout.java
@@ -0,0 +1,12 @@
+public class BorderLayout{
+	
+	public static void main(String ar[]){
+	 
+	 //tamaño de la ventana
+	 this.setBounds(10,40,350,280);
+	 //creacion y establecimiento del gestor de organización
+	 BorderLayout bd = new BorderLayout();
+	 this.setLayout(bd); 
+	
+	}//main
+}//class
